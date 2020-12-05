c775f96d02e8992a1599491e80d1b274561c41db
Jascha Muller <jascha.muller@gmail.com>
2020-12-05 12:24:35 +0100

    added server to allowed hosts

--- django_base/web_app_base/settings.py
+++ django_base/web_app_base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$zc-zouxr1ztwg$8b42i4*(kty2tidxbf6&k8^e0%$70i!lp8j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.100', 'localhost', '127.0.0.1', '78.46.151.217']
 
 
 # Application definition
